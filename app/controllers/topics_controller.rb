554861eb19466d3f813dde098e92cc498d77b7e8
Rachel Moshier <rmoshier@gmail.com>
2014-04-12 11:52:48 -0700

    Cleaning up some links.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -34,7 +34,7 @@ end
 
     respond_to do |format|
       if @topic.save
-        format.html { redirect_to @topic, notice: 'Topic was successfully created.' }
+        format.html { redirect_to topics_path, notice: 'Topic was successfully created.' }
         format.json { render action: 'show', status: :created, location: @topic }
       else
         format.html { render action: 'new' }
@@ -48,7 +48,7 @@ end
   def update
     respond_to do |format|
       if @topic.update(topic_params)
-        format.html { redirect_to @topic, notice: 'Topic was successfully updated.' }
+        format.html { redirect_to topics_path, notice: 'Topic was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
